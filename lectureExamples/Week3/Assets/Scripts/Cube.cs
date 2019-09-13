2ebcf5b5944c30e960fd8eb568c4e3f55f815ffb
Sahil Mayenkar <sahil.mayenkar@gmail.com>
2019-09-13 12:42:28 -0400

    Renamed project and updated example

--- lectureExamples/Week3/Assets/Scripts/Cube.cs
+++ lectureExamples/Week3/Assets/Scripts/Cube.cs
@@ -3,23 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Cube : MonoBehaviour
-
 {
-
 
     public float moveSpeed = 3, forceAmount = 400;
     public Rigidbody rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(translator());
+    }
+
+    private IEnumerator translator()
+    {
+        while (true)
+        {
+            transform.Translate(new Vector3(2.5f, 0, 0));
+            yield return new WaitForSeconds(2.0f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        transform.position = transform.position + new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        //transform.position = transform.position + new Vector3(moveSpeed * Time.deltaTime, 0, 0);
         //transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
         //rigidBody.MovePosition(transform.position + new Vector3(moveSpeed * Time.deltaTime, 0, 0));
 
@@ -36,5 +44,4 @@ public class Cube : MonoBehaviour
     {
         print("Collided with: " + collision.gameObject.name);
     }
-
 }
