32b1536ceb371f23b2d180c2475e18735fbbc657
Joseph Feldmann <josfeldmann@gmail.com>
2019-09-13 11:43:42 -0400

    added Lecture example 1

---
+++ lectureExamples/PhysicsAndRaycast/Assets/Scripts/RaycastFromCube.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastFromCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Input.GetMouseButton(0))
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, 1))
+            {
+
+                print("Hit object: " + hit.collider.gameObject.name);
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.green);
+            } else
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 100, Color.green);
+            }
+           
+
+
+        }
+
+    }
+}
